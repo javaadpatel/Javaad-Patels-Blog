660b23334aa78fc2c7defb2762269348ae895002
javaadpatel <javaadpatel@gmail.com>
2020-04-23 21:36:02 +0200

    intial commit with site configuration and styling

---
+++ src/components/common/Layout.js
@@ -0,0 +1,265 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Helmet from "react-helmet";
+import { Link, StaticQuery, graphql } from "gatsby";
+import Img from "gatsby-image";
+import { Image, Label } from "semantic-ui-react";
+import { SubscribeForm } from "../../components/common";
+
+import { Navigation } from ".";
+import config from "../../utils/siteConfig";
+
+// Styles
+import "../../styles/app.css";
+import "../../styles/semantic-ui/image.css";
+
+/**
+ * Main layout component
+ *
+ * The Layout component wraps around each page and template.
+ * It also provides the header, footer as well as the main
+ * styles, and meta data for each page.
+ *
+ */
+const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
+    const site = data.allGhostSettings.edges[0].node;
+    return (
+        <>
+            <Helmet>
+                <html lang={site.lang} />
+                <style type="text/css">{`${site.codeinjection_styles}`}</style>
+                <body className={bodyClass} />
+            </Helmet>
+
+            <div className="viewport">
+                <div className="viewport-top">
+                    {/* The main header section on top of the screen */}
+                    <header
+                        className="site-head"
+                        style={{
+                            ...(site.cover_image && {
+                                backgroundImage: `url(${site.cover_image})`,
+                            }),
+                        }}
+                    >
+                        <div className="container">
+                            <div className="site-mast">
+                                <div className="site-mast-left">
+                                    {!isHome ? (
+                                        <Link to="/">
+                                            <h3 className="site-title">
+                                                {site.title}
+                                            </h3>
+                                            {/* <img
+                                                className="site-logo"
+                                                src={site.logo}
+                                                alt={site.title}
+                                            /> */}
+                                        </Link>
+                                    ) : null}
+                                </div>
+                                {!isHome ? (
+                                    <div className="site-mast-right">
+                                        <a
+                                            href="https://www.linkedin.com/in/javaadpatel/"
+                                            className="site-nav-item"
+                                            target="_blank"
+                                            rel="noopener noreferrer"
+                                        >
+                                            <div>
+                                                <img
+                                                    className="site-nav-icon"
+                                                    src="/images/icons/linkedin.png"
+                                                    alt="LinkedIn"
+                                                />
+                                            </div>
+                                        </a>
+                                        <a
+                                            href="https://github.com/javaadpatel"
+                                            className="site-nav-item"
+                                            target="_blank"
+                                            rel="noopener noreferrer"
+                                        >
+                                            <div>
+                                                <img
+                                                    className="site-nav-icon"
+                                                    src="/images/icons/github.png"
+                                                    alt="Github"
+                                                />
+                                            </div>
+                                        </a>
+                                        <a
+                                            href="mailto:javaadpatel@gmail.com?subject=Interested in your work"
+                                            className="site-nav-item"
+                                            target="_blank"
+                                            rel="noopener noreferrer"
+                                        >
+                                            <div>
+                                                <img
+                                                    className="site-nav-icon"
+                                                    src="/images/icons/gmail.png"
+                                                    alt="Gmail"
+                                                />
+                                            </div>
+                                        </a>
+                                    </div>
+                                ) : null}
+                            </div>
+                            {isHome ? (
+                                <div className="site-banner">
+                                    <h1 className="site-banner-title">
+                                        <Image
+                                            src="/images/profile.jpg"
+                                            size="medium"
+                                            circular
+                                            centered
+                                            bordered
+                                        />
+                                        Hi!
+                                    </h1>
+                                    <p className="site-banner-desc">
+                                        My name is Javaad Patel. I'm a software
+                                        developer specializing in cloud
+                                        architecture, identity management and
+                                        distributed systems.
+                                    </p>
+                                    <br />
+                                    <div>
+                                        <a
+                                            href="https://www.linkedin.com/in/javaadpatel/"
+                                            className="home-social-item"
+                                            target="_blank"
+                                            rel="noopener noreferrer"
+                                        >
+                                            <div>
+                                                <img
+                                                    className="home-social-icon"
+                                                    src="/images/icons/linkedin.png"
+                                                    alt="LinkedIn"
+                                                />
+                                            </div>
+                                        </a>
+                                        <a
+                                            href="https://github.com/javaadpatel"
+                                            className="home-social-item"
+                                            target="_blank"
+                                            rel="noopener noreferrer"
+                                        >
+                                            <div>
+                                                <img
+                                                    className="home-social-icon"
+                                                    src="/images/icons/github.png"
+                                                    alt="Github"
+                                                />
+                                            </div>
+                                        </a>
+                                        <a
+                                            href="mailto:javaadpatel@gmail.com?subject=Interested in your work"
+                                            className="home-social-item"
+                                            target="_blank"
+                                            rel="noopener noreferrer"
+                                        >
+                                            <div>
+                                                <img
+                                                    className="home-social-icon"
+                                                    src="/images/icons/gmail.png"
+                                                    alt="Gmail"
+                                                />
+                                            </div>
+                                        </a>
+                                    </div>
+                                </div>
+                            ) : null}
+                            <nav className="site-nav">
+                                <div className="site-nav-left">
+                                    {/* The navigation items as setup in Ghost */}
+                                    <Navigation
+                                        data={site.navigation}
+                                        navClass="site-nav-item"
+                                    />
+                                </div>
+                                {/* <div className="site-nav-right">
+                                    <Link
+                                        className="site-nav-button"
+                                        to="/about"
+                                    >
+                                        About
+                                    </Link>
+                                </div> */}
+                            </nav>
+                        </div>
+                    </header>
+
+                    <main className="site-main">
+                        {/* All the main content gets inserted here, index.js, post.js */}
+                        {children}
+                        <SubscribeForm />
+                    </main>
+                </div>
+
+                <div className="viewport-bottom">
+                    {/* The footer at the very bottom of the screen */}
+                    <footer className="site-foot">
+                        <div className="site-foot-nav container">
+                            <div className="site-foot-nav-left">
+                                <Link to="/">{site.title}</Link> ©{" "}
+                                {new Date().getFullYear()} &mdash; Published
+                                with{" "}
+                                <a
+                                    className="site-foot-nav-item"
+                                    href="https://ghost.org"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    Ghost
+                                </a>
+                            </div>
+                            <div className="site-foot-nav-right">
+                                <Navigation
+                                    data={site.navigation}
+                                    navClass="site-foot-nav-item"
+                                />
+                            </div>
+                        </div>
+                    </footer>
+                </div>
+            </div>
+        </>
+    );
+};
+
+DefaultLayout.propTypes = {
+    children: PropTypes.node.isRequired,
+    bodyClass: PropTypes.string,
+    isHome: PropTypes.bool,
+    data: PropTypes.shape({
+        file: PropTypes.object,
+        allGhostSettings: PropTypes.object.isRequired,
+    }).isRequired,
+};
+
+const DefaultLayoutSettingsQuery = (props) => (
+    <StaticQuery
+        query={graphql`
+            query GhostSettings {
+                allGhostSettings {
+                    edges {
+                        node {
+                            ...GhostSettingsFields
+                        }
+                    }
+                }
+                file(relativePath: { eq: "ghost-icon.png" }) {
+                    childImageSharp {
+                        fixed(width: 30, height: 30) {
+                            ...GatsbyImageSharpFixed
+                        }
+                    }
+                }
+            }
+        `}
+        render={(data) => <DefaultLayout data={data} {...props} />}
+    />
+);
+
+export default DefaultLayoutSettingsQuery;
---
+++ src/components/common/SubscribeForm.js
@@ -0,0 +1,161 @@
+import React from "react";
+import { Form, Button, Card, Container, Grid, Icon } from "semantic-ui-react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
+import "../../styles/semantic-ui/form.css";
+import "../../styles/semantic-ui/grid.css";
+import "../../styles/semantic-ui/button.css";
+import "../../styles/semantic-ui/card.css";
+import "../../styles/semantic-ui/container.css";
+
+const SubscribeForm = () => {
+    const serialize = function (form) {
+        // Setup our serialized data
+        var serialized = "";
+
+        // Loop through each field in the form
+        for (var i = 0; i < form.elements.length; i++) {
+            var field = form.elements[i];
+
+            // Don't serialize fields without a name, submits, buttons, file and reset inputs, and disabled fields
+            if (
+                !field.name ||
+                field.disabled ||
+                field.type === "file" ||
+                field.type === "reset" ||
+                field.type === "submit" ||
+                field.type === "button"
+            )
+                continue;
+
+            // Convert field data to a query string
+            if (
+                (field.type !== "checkbox" && field.type !== "radio") ||
+                field.checked
+            ) {
+                serialized +=
+                    "&" +
+                    encodeURIComponent(field.name) +
+                    "=" +
+                    encodeURIComponent(field.value);
+            }
+        }
+
+        return serialized;
+    };
+
+    const displayMailChimpStatus = function (data) {
+        console.log(data);
+        // Get the status message content area
+        var mcStatus = document.querySelector(".mc-status");
+        if (!mcStatus) return;
+
+        // Update our status message
+        mcStatus.innerHTML = data.msg;
+
+        // If error, add error class
+        if (data.result === "error") {
+            mcStatus.classList.remove("success-message");
+            mcStatus.classList.add("error-message");
+            return;
+        }
+
+        // Otherwise, add success class
+        mcStatus.classList.remove("error-message");
+        mcStatus.classList.add("success-message");
+    };
+
+    const submitMailChimpForm = function (form) {
+        console.log(form);
+        // Get the Submit URL
+        var url = form.getAttribute("action");
+        url = url.replace("/post?u=", "/post-json?u=");
+        url += serialize(form) + "&c=displayMailChimpStatus";
+
+        // Create script with url and callback (if specified)
+        var script = window.document.createElement("script");
+        script.src = url;
+
+        // Insert script tag into the DOM (append to <head>)
+        var ref = window.document.getElementsByTagName("script")[0];
+        ref.parentNode.insertBefore(script, ref);
+
+        // After the script is loaded (and executed), remove it
+        script.onload = function () {
+            this.remove();
+        };
+    };
+
+    const onSubmit = (event) => {};
+
+    return (
+        <Container className="subscription-form-card">
+            <Grid className="centered" padded>
+                <Card fluid className="subscription-form-card">
+                    <Card.Content>
+                        <Container>
+                            <FontAwesomeIcon icon={faPaperPlane} size="2x" />
+                        </Container>
+                        <br />
+                        <Card.Header>Sign up for the latest news</Card.Header>
+                        <Card.Description>
+                            <Grid columns={1} padded="horizontally">
+                                <Grid.Column>
+                                    <Form
+                                        action="https://javaadpatel.us18.list-manage.com/subscribe/post?u=8e34a51ba7ad1ef1d8ecf5b2e&amp;id=ea72c8b591"
+                                        method="post"
+                                        id="mc-embedded-subscribe-form"
+                                        name="mc-embedded-subscribe-form"
+                                        // class="validate"
+                                        target="_blank"
+                                        onSubmit={onSubmit}
+                                    >
+                                        <Form.Field>
+                                            <input
+                                                placeholder="First Name"
+                                                name="FNAME"
+                                                id="mce-FNAME"
+                                            />
+                                        </Form.Field>
+                                        <Form.Field>
+                                            <input
+                                                type="email"
+                                                name="EMAIL"
+                                                id="mce-EMAIL"
+                                                placeholder="Enter Email Address"
+                                            />
+                                        </Form.Field>
+                                        <div
+                                            style={{
+                                                position: "absolute",
+                                                left: "-5000px",
+                                            }}
+                                            aria-hidden="true"
+                                        >
+                                            <input
+                                                type="text"
+                                                name="b_f2d244c0df42a0431bd08ddea_aeaa9dd034"
+                                                tabIndex="-1"
+                                            />
+                                        </div>
+                                        <div className="mc-status"></div>
+                                        <Button
+                                            fluid
+                                            type="submit"
+                                            color="black"
+                                            id="mc-embedded-subscribe"
+                                        >
+                                            Subscribe Now
+                                        </Button>
+                                    </Form>
+                                </Grid.Column>
+                            </Grid>
+                        </Card.Description>
+                    </Card.Content>
+                </Card>
+            </Grid>
+        </Container>
+    );
+};
+
+export default SubscribeForm;
