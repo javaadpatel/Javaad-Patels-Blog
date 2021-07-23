7110fc5c86f392caec73720f8d20444162bba9c0
javaadpatel <javaadpatel@gmail.com>
2021-07-23 17:50:54 +0200

    updated postsPerPage to 18

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,9 +1,9 @@
 module.exports = {
     siteUrl: `https://javaadpatel.com`, // Site domain. Do not include a trailing slash!
 
-    postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
+    postsPerPage: 18, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
-    googleAnalyticsBaseUrl: 'https://javaad-google-analytics.netlify.app',
+    googleAnalyticsBaseUrl: "https://javaad-google-analytics.netlify.app",
 
     siteTitleMeta: `Javaad Patel's Blog`, // This allows an alternative site title for meta data for pages.
     siteDescriptionMeta: `Blog posts about cloud architecture, identity management and distributed systems`, // This allows an alternative site description for meta data for pages.
@@ -15,4 +15,4 @@ module.exports = {
     siteIcon: `favicon.png`, // Logo in /static dir used for SEO, RSS, and App manifest
     backgroundColor: `#e9e9e9`, // Used for Offline Manifest
     themeColor: `#15171A`, // Used for Offline Manifest
-}
+};
